4cbb599e3fb06f54062a007015fde4ea7709f096
Paul Blackwell <blackwellpaul34@gmail.com>
2020-09-01 08:17:47 +0100

    Project setup

---
+++ github-jobs-app/src/JobsApp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles/components/__JobsApp.scss';
+
+export default function JobsApp() {
+    return (
+        <div className="JobsApp">
+            <h1 className="JobsApp__title">Github Jobs</h1>
+        </div>
+    )
+}
