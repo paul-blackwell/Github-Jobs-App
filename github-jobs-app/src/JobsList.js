d1e53f5b0a092dd5c894826db23fb9bd72686d0c
Paul Blackwell <blackwellpaul34@gmail.com>
2020-09-09 08:17:32 +0100

    JobsList now works for when their is no jobs

--- github-jobs-app/src/JobsList.js
+++ github-jobs-app/src/JobsList.js
@@ -7,15 +7,26 @@ import './styles/components/__JobList.scss'
 export default function JobsList() {
 
     const { jobs } = useContext(JobsContext);
+    console.log(jobs);
 
-    return (
-        <div className="JobsList">
-            <h2 className="JobsList__title">Showing 2 jobs</h2>
-            <ul className="JobsList__list">
-                {jobs.map((job) => (
-                    <Job {...job} key={job.id}/>
-                ))}
-            </ul>
-        </div>
-    )
+    if (jobs) {
+        return (
+            <div className="JobsList">
+                <h2 className="JobsList__title">Showing {jobs.length} jobs</h2>
+                {jobs.length > 0 &&
+                    <ul className="JobsList__list">
+                        {jobs.map((job) => (
+                            <Job {...job} key={job.id} />
+                        ))}
+                    </ul>
+                }
+            </div>
+        )
+    } else {
+        return (
+            <div className="JobsList">
+                <h2 className="JobsList__title">Showing 0 jobs</h2>
+            </div>
+        )
+    }
 }
--- github-jobs-app/src/context/jobs.context.js
+++ github-jobs-app/src/context/jobs.context.js
@@ -2,7 +2,7 @@ import React, { createContext } from "react";
 import useJobState from '../hooks/useJobState';
 
 
-const defaultJobs = [];
+const defaultJobs = '';
 
 export const JobsContext = createContext();
 
